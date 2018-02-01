097d4bba991a78b8d1bedf7c2d5101564983a031
Java4Life <liuguojian@outlook.com>
2018-02-01 13:36:15 +0800

    change titles

--- app/src/main/java/com/wuxudu/android/MainActivity.java
+++ app/src/main/java/com/wuxudu/android/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_plan);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_explore);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_me);
                     return true;
             }
             return false;
